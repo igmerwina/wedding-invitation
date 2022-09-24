73f5a0b0659ea58f7d3a77a393c0ac8ab24ee354
P93694-dev <erwin.ardiantha@pegadaian.co.id>
2022-09-24 17:23:03 +0700

    [DEV] - fixing padding on bride and groom. fixing message title bg color

--- app/components/BrideAndGroom.tsx
+++ app/components/BrideAndGroom.tsx
@@ -8,7 +8,7 @@ const BrideAndGroom = () => {
       <h3 className="text-4xl text-center font-sans font-bold text-gray-700 mb-6">
       <TextWithLine>Kami yang Berbahagia</TextWithLine>
       </h3>
-      <div className="w-full flex flex-wrap pb-20">
+      <div className="w-full flex flex-wrap pb-2">
         <div className="w-full md:w-1/2 px-4">
           <div className="flex flex-col items-center py-14 sm:px-8 lg:p-20 text-center">
             <img
--- app/components/Massage.tsx
+++ app/components/Massage.tsx
@@ -2,6 +2,7 @@ import { Fragment, memo, useEffect, useRef, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import { useFetcher, useLoaderData } from "remix";
 import { LoaderDataType } from "~/controls";
+import TextWithLine from "./Utils/TextWithLine";
 
 const PER_PAGE = 5;
 
@@ -50,135 +51,140 @@ const Message = memo(() => {
   }, [fetcher]);
 
   return (
-    <div className="w-full flex flex-wrap">
-      <div className="w-full md:w-1/2 px-4">
-        <div className="rounded-lg shadow-lg p-6 mt-10 md:p-8 bg-white top-8 mb-6 md:mb-10">
-          <fetcher.Form replace method="post" ref={formRef}>
-            <fieldset disabled={fetcher.state === "submitting"}>
-              <input
-                className="w-full bg-[#f8fafc] rounded border px-2 py-1.5 mb-6 outline-[#CE7BB0]"
-                placeholder="Nama Anda"
-                name="name"
-                required
-                maxLength={30}
-              />
-              <textarea
-                className="w-full bg-[#f8fafc] rounded border px-2 py-1.5 min-h-[6rem] mb-4 outline-[#CE7BB0]"
-                placeholder="Doa/Ucapan bagi kami"
-                name="message"
-                required
-                maxLength={200}
-              />
-              <div className="flex justify-end">
-                <button
-                  type="submit"
-                  className="px-5 py-2 transition-all text-lg font-semibold rounded-md bg-[#CE7BB0] hover:bg-[#A267AC] outline-[#6867AC] text-white outline-4 disabled:opacity-50 disabled:pointer-events-none cursor-pointer"
-                >
-                  Kirim
-                </button>
-              </div>
-            </fieldset>
-
-            {fetcher.data?.ok === false && (
-              <p className="pt-4 text-red-500">Gagal mengirim pesan.</p>
-            )}
-          </fetcher.Form>
-        </div>
+    <div>
+      <div className="text-3xl font-head font-bold text-center mb-10 pt-10 text-gray-700">
+        <h4 className="mb-1"><TextWithLine>Berikan Doa Terbaik Untuk Kami</TextWithLine></h4>
       </div>
+      <div className="w-full flex flex-wrap">
+        <div className="w-full md:w-1/2 px-4">
+          <div className="rounded-lg shadow-lg p-6 mt-10 md:p-8 bg-white top-8 mb-6 md:mb-10">
+            <fetcher.Form replace method="post" ref={formRef}>
+              <fieldset disabled={fetcher.state === "submitting"}>
+                <input
+                  className="w-full bg-[#f8fafc] rounded border px-2 py-1.5 mb-6 outline-[#CE7BB0]"
+                  placeholder="Nama Anda"
+                  name="name"
+                  required
+                  maxLength={30}
+                />
+                <textarea
+                  className="w-full bg-[#f8fafc] rounded border px-2 py-1.5 min-h-[6rem] mb-4 outline-[#CE7BB0]"
+                  placeholder="Doa/Ucapan bagi kami"
+                  name="message"
+                  required
+                  maxLength={200}
+                />
+                <div className="flex justify-end">
+                  <button
+                    type="submit"
+                    className="px-5 py-2 transition-all text-lg font-semibold rounded-md bg-[#CE7BB0] hover:bg-[#A267AC] outline-[#6867AC] text-white outline-4 disabled:opacity-50 disabled:pointer-events-none cursor-pointer"
+                  >
+                    Kirim
+                  </button>
+                </div>
+              </fieldset>
 
-      <div className="w-full md:w-1/2 px-4" ref={messagesRef}>
-        <div className="pt-4">
-          {fetcher.state === "loading" && (
-            <div className="mb-5">
-              <h5>
-                <strong>
-                  <Skeleton width={120} />
-                </strong>
-              </h5>
-              <p>
-                <Skeleton width={200} />
-              </p>
+              {fetcher.data?.ok === false && (
+                <p className="pt-4 text-red-500">Gagal mengirim pesan.</p>
+              )}
+            </fetcher.Form>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2 px-4" ref={messagesRef}>
+          <div className="pt-4">
+            {fetcher.state === "loading" && (
+              <div className="mb-5">
+                <h5>
+                  <strong>
+                    <Skeleton width={120} />
+                  </strong>
+                </h5>
+                <p>
+                  <Skeleton width={200} />
+                </p>
+              </div>
+            )}
+
+            {messagesDisplay.map((message, i) => (
+              <div className="mb-7 mt-8 md:mb-5" key={i}>
+                <h5 className="font-semibold font-sans text-gray-700 mb-1">
+                  📩 {message.name}
+                </h5>
+                <p className="whitespace-pre-wrap font-sans text-gray-800 leading-6 border-b-2 pl-6 py-1.5">
+                  {message.message}
+                </p>
+              </div>
+            ))}
+          </div>
+
+          {/* PAGINATION */}
+          {totalPages === 1 ? null : (
+            <div className="flex items-center justify-center pl-2 pt-4">
+              <button
+                className="mr-2 text-2xl text-gray-700 transition-all hover:text-[#CE7BB0] disabled:opacity-50 disabled:hover:text-gray-700 pt-1"
+                onClick={() => onChangePage(page - 1)}
+                disabled={page === 0}
+                aria-label="page-left"
+              >
+                <i className="bx bx-chevron-left" />
+              </button>
+
+              {paginationDisplay[0] > 0 ? (
+                <Fragment>
+                  <button
+                    className={
+                      "mr-2 px-1 font-semibold text-lg text-gray-700 transition-all hover:text-[#CE7BB0]"
+                    }
+                    onClick={() => onChangePage(0)}
+                  >
+                    1
+                  </button>
+                  <div className="font-semibold text-lg mr-2">...</div>
+                </Fragment>
+              ) : null}
+
+              {paginationDisplay.map((num) => (
+                <button
+                  className={
+                    "mr-2 px-1 font-semibold text-lg " +
+                    (num === page
+                      ? "text-[#CE7BB0]"
+                      : "text-gray-700 transition-all hover:text-[#CE7BB0]")
+                  }
+                  key={num}
+                  onClick={() => onChangePage(num)}
+                >
+                  {num + 1}
+                </button>
+              ))}
+
+              {paginationDisplay[paginationDisplay.length - 1] <
+                totalPages - 1 ? (
+                <Fragment>
+                  <div className="font-semibold text-lg mr-2">...</div>
+                  <button
+                    className={
+                      "mr-2 px-1 font-semibold text-lg text-gray-700 transition-all hover:text-[#CE7BB0]"
+                    }
+                    onClick={() => onChangePage(totalPages - 1)}
+                  >
+                    {totalPages}
+                  </button>
+                </Fragment>
+              ) : null}
+
+              <button
+                className="text-2xl mr-2 text-gray-700 transition-all hover:text-[#CE7BB0] disabled:opacity-50 disabled:hover:text-gray-700 pt-1"
+                onClick={() => onChangePage(page + 1)}
+                disabled={page === totalPages - 1}
+                aria-label="page-right"
+              >
+                <i className="bx bx-chevron-right" />
+              </button>
             </div>
           )}
-
-          {messagesDisplay.map((message, i) => (
-            <div className="mb-7 mt-8 md:mb-5" key={i}>
-              <h5 className="font-semibold font-sans text-gray-700 mb-1">
-                📩 {message.name}
-              </h5>
-              <p className="whitespace-pre-wrap font-sans text-gray-800 leading-6 border-b-2 pl-6 py-1.5">
-                {message.message}
-              </p>
-            </div>
-          ))}
         </div>
-
-        {/* PAGINATION */}
-        {totalPages === 1 ? null : (
-          <div className="flex items-center justify-center pl-2 pt-4">
-            <button
-              className="mr-2 text-2xl text-gray-700 transition-all hover:text-[#CE7BB0] disabled:opacity-50 disabled:hover:text-gray-700 pt-1"
-              onClick={() => onChangePage(page - 1)}
-              disabled={page === 0}
-              aria-label="page-left"
-            >
-              <i className="bx bx-chevron-left" />
-            </button>
-
-            {paginationDisplay[0] > 0 ? (
-              <Fragment>
-                <button
-                  className={
-                    "mr-2 px-1 font-semibold text-lg text-gray-700 transition-all hover:text-[#CE7BB0]"
-                  }
-                  onClick={() => onChangePage(0)}
-                >
-                  1
-                </button>
-                <div className="font-semibold text-lg mr-2">...</div>
-              </Fragment>
-            ) : null}
-
-            {paginationDisplay.map((num) => (
-              <button
-                className={
-                  "mr-2 px-1 font-semibold text-lg " +
-                  (num === page
-                    ? "text-[#CE7BB0]"
-                    : "text-gray-700 transition-all hover:text-[#CE7BB0]")
-                }
-                key={num}
-                onClick={() => onChangePage(num)}
-              >
-                {num + 1}
-              </button>
-            ))}
-
-            {paginationDisplay[paginationDisplay.length - 1] <
-              totalPages - 1 ? (
-              <Fragment>
-                <div className="font-semibold text-lg mr-2">...</div>
-                <button
-                  className={
-                    "mr-2 px-1 font-semibold text-lg text-gray-700 transition-all hover:text-[#CE7BB0]"
-                  }
-                  onClick={() => onChangePage(totalPages - 1)}
-                >
-                  {totalPages}
-                </button>
-              </Fragment>
-            ) : null}
-
-            <button
-              className="text-2xl mr-2 text-gray-700 transition-all hover:text-[#CE7BB0] disabled:opacity-50 disabled:hover:text-gray-700 pt-1"
-              onClick={() => onChangePage(page + 1)}
-              disabled={page === totalPages - 1}
-              aria-label="page-right"
-            >
-              <i className="bx bx-chevron-right" />
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
